46d28a4653320474e7089f127ca6ee037a2cffcf
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2025-01-22 19:31:45 +0200

    02.todos.18_02: fix errors in todos_reducer used in combined reducer

--- 07.redux/02.todos.18_02/src/store/todos/todos_reducer.js
+++ 07.redux/02.todos.18_02/src/store/todos/todos_reducer.js
@@ -1,49 +1,36 @@
 import { ADD_TODO, TOGGLE_TODO, UPDATE_TODO, DELETE_TODO } from './todos_const';
 
-const persistTodos = [{ id: -100, text: 'text -100', completed: false }];
-const initialState = { todos: persistTodos };
+const persistTodos = [{ id: 1, text: 'text 1', completed: false }];
 
-export const todos = (state = initialState, action) => {
+export const todos = (state = persistTodos, action) => {
     switch (action.type) {
         case ADD_TODO:
-            return {
+            return [
                 ...state,
-                todos: [
-                    ...state.todos,
-                    {
-                        id: Date.now(),
-                        text: action.text,
-                        completed: false,
-                    },
-                ],
-            };
+                {
+                    id: Date.now(),
+                    text: action.text,
+                    completed: false,
+                },
+            ];
         case TOGGLE_TODO:
-            return {
-                ...state,
-                todos: state.todos.map((todo) => {
-                    if (todo.id !== action.id) {
-                        return todo;
-                    } else {
-                        return { ...todo, completed: !todo.completed };
-                    }
-                }),
-            };
+            return state.map((todo) => {
+                if (todo.id !== action.id) {
+                    return todo;
+                } else {
+                    return { ...todo, completed: !todo.completed };
+                }
+            });
         case UPDATE_TODO:
-            return {
-                ...state,
-                todos: state.todos.map((todo) => {
-                    if (todo.id !== action.id) {
-                        return todo;
-                    } else {
-                        return { ...todo, text: action.text };
-                    }
-                }),
-            };
+            return state.map((todo) => {
+                if (todo.id !== action.id) {
+                    return todo;
+                } else {
+                    return { ...todo, text: action.text };
+                }
+            });
         case DELETE_TODO:
-            return {
-                ...state,
-                todos: state.todos.filter((todo) => todo.id !== action.id),
-            };
+            return state.filter((todo) => todo.id !== action.id);
         default:
             return state;
     }
