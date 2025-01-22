7acc92c6f94025c1e96aa1201c2b3ebb6ec35f51
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2025-01-22 18:48:41 +0200

    02.todos.18_02: 22 architecture variant1 3 configure store

--- 07.redux/02.todos.18_02/src/main.jsx
+++ 07.redux/02.todos.18_02/src/main.jsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
-import { store } from './store';
+import { configureStore } from './store';
+
+const store = configureStore();
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
--- 07.redux/02.todos.18_02/src/store/index.js
+++ 07.redux/02.todos.18_02/src/store/index.js
@@ -2,4 +2,6 @@ import { createStore } from 'redux';
 import { devToolsEnhancer } from '@redux-devtools/extension';
 import { rootReducer } from './root_reducer';
 
-export const store = createStore(rootReducer, devToolsEnhancer());
+export const configureStore = () => {
+    return createStore(rootReducer, devToolsEnhancer());
+};
