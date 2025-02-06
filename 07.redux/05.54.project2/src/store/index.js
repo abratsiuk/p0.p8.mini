a062216dea4bc2c4a5c518be0eaab97eb3252ee1
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2025-02-06 18:50:26 +0200

    05.54.project2 56

---
+++ 07.redux/05.54.project2/src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import { rootReducer } from './root-reducer';
+import thunk from 'redux-thunk';
+import axios from 'axios';
+import * as api from '../config';
+
+export const store = createStore(
+    rootReducer,
+    composeWithDevTools(
+        applyMiddleware(
+            thunk.withExtraArgument({
+                client: axios,
+                api,
+            })
+        )
+    )
+);
