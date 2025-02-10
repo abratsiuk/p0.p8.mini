4c0099b03d11230af32a756c5c383c7181bc687c
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2025-02-10 22:08:18 +0200

    59, 60

--- 07.redux/05.54.project2/src/pages/HomePage.jsx
+++ 07.redux/05.54.project2/src/pages/HomePage.jsx
@@ -1,48 +1,70 @@
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import { useSelector, useDispatch } from 'react-redux';
+import { selectSearch } from '../store/controls/controls-selectors';
 import { List } from '../components/List';
 import { Card } from '../components/Card';
 import { Controls } from '../components/Controls';
+import {
+    selectCountriesInfo,
+    selectVisibleCountries,
+} from '../store/countries/countries-selectors';
+import { loadCountries } from '../store/countries/countries-actions';
 
 export const HomePage = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const countries = [];
+    const search = useSelector(selectSearch);
+    const countries = useSelector((state) =>
+        selectVisibleCountries(state, { search })
+    );
+    const { status, error, qty } = useSelector(selectCountriesInfo);
+    const dispatch = useDispatch();
 
-  return (
-    <>
-      <Controls />
+    useEffect(() => {
+        if (qty === 0) {
+            dispatch(loadCountries());
+        }
+    }, [qty, dispatch]);
 
-      <List>
-            {countries.map((c) => {
-              const countryInfo = {
-                img: c.flags.png,
-                name: c.name,
-                info: [
-                  {
-                    title: 'Population',
-                    description: c.population.toLocaleString(),
-                  },
-                  {
-                    title: 'Region',
-                    description: c.region,
-                  },
-                  {
-                    title: 'Capital',
-                    description: c.capital,
-                  },
-                ],
-              };
+    return (
+        <>
+            <Controls />
 
-              return (
-                <Card
-                  key={c.name}
-                  onClick={() => navigate(`/country/${c.name}`)}
-                  {...countryInfo}
-                />
-              );
-            })}
-          </List>
-    </>
-  );
+            {error && <h2>{error}</h2>}
+            {status === 'loading' && <h2>Loading...</h2>}
+            {status === 'received' && (
+                <List>
+                    {countries.map((c) => {
+                        const countryInfo = {
+                            img: c.flags.png,
+                            name: c.name,
+                            info: [
+                                {
+                                    title: 'Population',
+                                    description: c.population.toLocaleString(),
+                                },
+                                {
+                                    title: 'Region',
+                                    description: c.region,
+                                },
+                                {
+                                    title: 'Capital',
+                                    description: c.capital,
+                                },
+                            ],
+                        };
+
+                        return (
+                            <Card
+                                key={c.name}
+                                onClick={() => navigate(`/country/${c.name}`)}
+                                {...countryInfo}
+                            />
+                        );
+                    })}
+                </List>
+            )}
+        </>
+    );
 };
--- 07.redux/05.54.project2/src/store/root-reducer.js
+++ 07.redux/05.54.project2/src/store/root-reducer.js
@@ -1,5 +1,10 @@
 import { combineReducers } from 'redux';
 import { themeReducer } from '../store/theme/theme-reducer';
+import { countriesReducer } from '../store/countries/countries-reducer';
+import { controlsReducer } from './controls/controls-reducer';
+
 export const rootReducer = combineReducers({
     theme: themeReducer,
+    countries: countriesReducer,
+    controls: controlsReducer,
 });
