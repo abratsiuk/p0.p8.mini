2350e56a4eded0fea0599a8475e3cc55b1aa086a
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2025-03-12 14:36:41 +0200

    add getTodos

--- 07.redux/08.81ex.09-extra-reducers/src/features/Todos/TodoList.jsx
+++ 07.redux/08.81ex.09-extra-reducers/src/features/Todos/TodoList.jsx
@@ -1,26 +1,39 @@
-import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-import {selectVisibleTodos, toggleTodo, removeTodo} from './todos-slice';
-
+import {
+    selectVisibleTodos,
+    toggleTodo,
+    removeTodo,
+    getTodos,
+} from './todos-slice';
 
 export const TodoList = () => {
-  const activeFilter = useSelector(state => state.filter)
-  const todos = useSelector(state => selectVisibleTodos(state, activeFilter));
-  const dispatch = useDispatch();
+    const activeFilter = useSelector((state) => state.filter);
+    const todos = useSelector((state) =>
+        selectVisibleTodos(state, activeFilter)
+    );
+    const dispatch = useDispatch();
 
-  return (
-    <ul>
-      {todos.map((todo) => (
-        <li key={todo.id}>
-          <input
-            type="checkbox"
-            checked={todo.completed}
-            onChange={() => dispatch(toggleTodo(todo.id))}
-          />{" "}
-          {todo.title}{" "}
-          <button onClick={() => dispatch(removeTodo(todo.id))}>delete</button>
-        </li>
-      ))}
-    </ul>
-  );
+    useEffect(() => {
+        dispatch(getTodos());
+    }, [dispatch]);
+
+    return (
+        <ul>
+            {todos.map((todo) => (
+                <li key={todo.id}>
+                    <input
+                        type="checkbox"
+                        checked={todo.completed}
+                        onChange={() => dispatch(toggleTodo(todo.id))}
+                    />{' '}
+                    {todo.title}{' '}
+                    <button onClick={() => dispatch(removeTodo(todo.id))}>
+                        delete
+                    </button>
+                </li>
+            ))}
+        </ul>
+    );
 };
