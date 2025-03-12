4a834fed8a5aaf58312d1db63b3e445cb0a92aa4
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2025-03-12 15:40:23 +0200

    restore initial, I will execute against

--- 07.redux/08.81ex.09-extra-reducers/src/features/Todos/TodoList.jsx
+++ 07.redux/08.81ex.09-extra-reducers/src/features/Todos/TodoList.jsx
@@ -1,39 +1,26 @@
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 
-import {
-    selectVisibleTodos,
-    toggleTodo,
-    removeTodo,
-    getTodos,
-} from './todos-slice';
+import {selectVisibleTodos, toggleTodo, removeTodo} from './todos-slice';
+
 
 export const TodoList = () => {
-    const activeFilter = useSelector((state) => state.filter);
-    const todos = useSelector((state) =>
-        selectVisibleTodos(state, activeFilter)
-    );
-    const dispatch = useDispatch();
+  const activeFilter = useSelector(state => state.filter)
+  const todos = useSelector(state => selectVisibleTodos(state, activeFilter));
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(getTodos());
-    }, [dispatch]);
-
-    return (
-        <ul>
-            {todos.map((todo) => (
-                <li key={todo.id}>
-                    <input
-                        type="checkbox"
-                        checked={todo.completed}
-                        onChange={() => dispatch(toggleTodo(todo.id))}
-                    />{' '}
-                    {todo.title}{' '}
-                    <button onClick={() => dispatch(removeTodo(todo.id))}>
-                        delete
-                    </button>
-                </li>
-            ))}
-        </ul>
-    );
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          <input
+            type="checkbox"
+            checked={todo.completed}
+            onChange={() => dispatch(toggleTodo(todo.id))}
+          />{" "}
+          {todo.title}{" "}
+          <button onClick={() => dispatch(removeTodo(todo.id))}>delete</button>
+        </li>
+      ))}
+    </ul>
+  );
 };
