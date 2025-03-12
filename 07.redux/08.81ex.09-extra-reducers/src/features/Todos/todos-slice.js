7ae0dbe6eff4905c87c1e7fc152d689133b85b8b
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2025-03-12 19:14:49 +0200

    add loadTodos

--- 07.redux/08.81ex.09-extra-reducers/src/features/Todos/todos-slice.js
+++ 07.redux/08.81ex.09-extra-reducers/src/features/Todos/todos-slice.js
@@ -1,78 +1,84 @@
-import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-import {resetToDefault} from '../Reset/reset-action';
+import { resetToDefault } from '../Reset/reset-action';
 
-export const createTodo = createAsyncThunk(
-  '@@todos/create-todo',
-  async (title) => {
-    const res = await fetch('http://localhost:3001/todos', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({title, completed: false})
-    })
+export const loadTodos = createAsyncThunk('@@todos/load-todos', async () => {
+    const res = await fetch('http://localhost:3001/todos');
     const data = await res.json();
-    
     return data;
-  }
+});
+export const createTodo = createAsyncThunk(
+    '@@todos/create-todo',
+    async (title) => {
+        const res = await fetch('http://localhost:3001/todos', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ title, completed: false }),
+        });
+        const data = await res.json();
+
+        return data;
+    }
 );
 
-
 const todoSlice = createSlice({
-  name: '@@todos',
-  initialState: {
-    entities: [],
-    loading: 'idle', // 'loading',
-    error: null,
-  },
-  reducers: {
-    removeTodo: (state, action) => {
-      const id = action.payload;
-      return state.filter((todo) => todo.id !== id);
+    name: '@@todos',
+    initialState: {
+        entities: [],
+        loading: 'idle', // 'loading',
+        error: null,
     },
-    toggleTodo: (state, action) => {
-      const id = action.payload;
-      const todo = state.find((todo) => todo.id === id);
-      todo.completed = !todo.completed;
+    reducers: {
+        removeTodo: (state, action) => {
+            const id = action.payload;
+            return state.filter((todo) => todo.id !== id);
+        },
+        toggleTodo: (state, action) => {
+            const id = action.payload;
+            const todo = state.find((todo) => todo.id === id);
+            todo.completed = !todo.completed;
+        },
     },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(resetToDefault, () => {
-        return []
-      })
-      .addCase(createTodo.pending, (state, action) => {
-        state.loading = 'loading';
-        state.error = null;
-      })
-      .addCase(createTodo.rejected, (state) => {
-        state.loading = 'idle';
-        state.error = 'Something went wrong!'
-      })
-      .addCase(createTodo.fulfilled, (state, action) => {
-        state.entities.push(action.payload)
-      })
-  }
+    extraReducers: (builder) => {
+        builder
+            .addCase(resetToDefault, () => {
+                return [];
+            })
+            .addCase(loadTodos.pending, (state, action) => {
+                state.loading = 'loading';
+                state.error = null;
+            })
+            .addCase(loadTodos.rejected, (state) => {
+                state.loading = 'idle';
+                state.error = 'Something went wrong!';
+            })
+            .addCase(loadTodos.fulfilled, (state, action) => {
+                state.entities = action.payload;
+            })
+            .addCase(createTodo.fulfilled, (state, action) => {
+                state.entities.push(action.payload);
+            });
+    },
 });
-export const {addTodo, removeTodo, toggleTodo} = todoSlice.actions;
+export const { addTodo, removeTodo, toggleTodo } = todoSlice.actions;
 
 export const todoReducer = todoSlice.reducer;
 
-
 export const selectVisibleTodos = (state, filter) => {
-  switch (filter) {
-    case 'all': {
-      return state.todos.entities;
+    switch (filter) {
+        case 'all': {
+            return state.todos.entities;
+        }
+        case 'active': {
+            return state.todos.entities.filter((todo) => !todo.completed);
+        }
+        case 'completed': {
+            return state.todos.entities.filter((todo) => todo.completed);
+        }
+        default: {
+            return state.todos;
+        }
     }
-    case 'active': {
-      return state.todos.entities.filter(todo => !todo.completed);
-    }
-    case 'completed': {
-      return state.todos.entities.filter(todo => todo.completed);
-    }
-    default: {
-      return state.todos;
-    }
-  }
-}
+};
