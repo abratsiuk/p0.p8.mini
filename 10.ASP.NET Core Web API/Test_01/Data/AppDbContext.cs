8e5e8050aba96e3588b074f8a10e869c79090190
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2025-03-14 18:08:10 +0200

    create .NET solution: connect to database, create model and controller and migration

---
+++ 10.ASP.NET Core Web API/Test_01/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test_01.Models;
+
+namespace Test_01.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        // Добавим DbSet для сущностей позже
+        public DbSet<Product> Products { get; set; }
+    }
+}
