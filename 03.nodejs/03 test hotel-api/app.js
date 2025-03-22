12bc3f3dc39ad6975f68150eec7d01e634c73e5f
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2025-03-22 12:58:55 +0200

    ...

---
+++ 03.nodejs/03 test hotel-api/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hotel booking API работает!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на порту ${PORT}`);
+});
